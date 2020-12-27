a29644740552c82593e34d0c41718c5c91db20f1
Ángel Pérez <angelpxrz@gmail.com>
2020-12-27 01:53:31 +0100

    postComment function

---
+++ functions/index.js
@@ -0,0 +1,28 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+admin.initializeApp();
+
+exports.postComment = functions.https.onCall(async (data, context) => {
+
+    const db = admin.firestore();
+    const snapshot = await db
+        .collection('publicProfiles')
+        .where('userId', '==', context.auth.uid)
+        .limit(1)
+        .get();
+
+    await db.collection('comments').add({
+        text: data.text,
+        username: snapshot.docs[0].id,
+        dateCreated: new Date(),
+        book: db.collection('books').doc(data.bookId)
+    })
+});
